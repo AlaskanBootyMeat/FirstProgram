8e6cac2c3d1d22744b483cf1f760c144f256c13d
AlaskanBootyMeat <cstewd@gmail.com>
2023-03-18 15:58:45 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+
+
+void PrintHello(string message)
+{
+    var finalmessage = "Hello," + " " + message;
+    Console.WriteLine(finalmessage);
+}
+
+string GetName()
+{
+//     string firstname; // Anonymous variable until a value is assigned
+//     // Console.WriteLine(firstname);
+//     // var firstname = "shon";
+//     firstname = "shon";
+//     firstname = "chase";
+
+//     string lastname = "stewman";
+
+//     string fullname = firstname + " " + lastname; // Expression that combines the first name, an empty space, and a last name
+//     return fullname;
+    Console.WriteLine("what is your name");
+    var name = Console.ReadLine();
+    return name;
+}
+
+string fullname = GetName();
+PrintHello(fullname);
